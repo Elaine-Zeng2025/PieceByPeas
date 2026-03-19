d56850a06e542d491980b08cf7086e56b2fb47b5
Elaine-Zeng2025 <elzeng07@gmail.com>
2026-03-18 21:52:59 -0400

    Change to try render

--- backend/app.py
+++ backend/app.py
@@ -34,4 +34,5 @@ def serve_static(path):
     return send_from_directory(app.static_folder, path)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=not IS_PROD, host='0.0.0.0', port=port)
